89c3ea3f7e427af9c8c9fe1177e0adb6b0425871
David Chavez <david@dcvz.io>
2020-09-26 14:35:41 +0200

    Update install script to use Github releases

--- install.sh
+++ install.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+function get_download_url {
+	curl -s https://api.github.com/repos/DoubleSymmetry/lynx/releases/latest | grep browser_download_url | grep lynx | cut -d '"' -f 4
+}
+
 echo "Installing lynx"
-curl -LSs --output /usr/local/bin/lynx https://raw.githubusercontent.com/DoubleSymmetry/lynx/master/bin/lynx
+curl -LSs --output /usr/local/bin/lynx $(get_download_url)
 chmod +x /usr/local/bin/lynx
 echo "lynx installed. Try running 'lynx'"
